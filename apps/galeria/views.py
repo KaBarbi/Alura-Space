c4dcdfc40f2b9f816a0966179e2e33409dbf6975
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-07-24 15:12:41 -0300

    editando fotografias

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -5,24 +5,29 @@ from apps.galeria.forms import FotografiaForms
 
 from django.contrib import messages
 
+
 def index(request):
     if not request.user.is_authenticated:
         messages.error(request, 'Usuário não logado')
         return redirect('login')
 
-    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+    fotografias = Fotografia.objects.order_by(
+        "data_fotografia").filter(publicada=True)
     return render(request, 'galeria/index.html', {"cards": fotografias})
+
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+
 
 def buscar(request):
     if not request.user.is_authenticated:
         messages.error(request, 'Usuário não logado')
         return redirect('login')
 
-    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+    fotografias = Fotografia.objects.order_by(
+        "data_fotografia").filter(publicada=True)
 
     if "buscar" in request.GET:
         nome_a_buscar = request.GET['buscar']
@@ -30,6 +35,7 @@ def buscar(request):
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
     return render(request, "galeria/buscar.html", {"cards": fotografias})
+
 
 def nova_imagem(request):
     if not request.user.is_authenticated:
@@ -46,8 +52,20 @@ def nova_imagem(request):
 
     return render(request, 'galeria/nova_imagem.html', {'form': form})
 
-def editar_imagem(request):
-    pass
+
+def editar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    form = Fotografia(isinstance=fotografia)
+
+    if request.method == 'POST':
+        form = FotografiaForms(
+            request.POST, request.FILES, instance=fotografia)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Fotografia editada com sucesso!')
+            return redirect('index')
+    return render(render, 'galeria/editar_imagem.html', {'form': form, 'foto_id': foto_id})
+
 
 def deletar_imagem(request):
     pass
